92858f99f44df8ec591bd27fa227fc49595a33d7
Christian Blakely <clisztian@gmail.com>
2021-11-09 18:41:23 +0100

    shared clauses

--- src/main/java/examples/TimeSeriesClassification.java
+++ src/main/java/examples/TimeSeriesClassification.java
@@ -20,6 +20,7 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import plots.KPIPlot;
 import records.TimeTrois;
+import tsetlin.AutomataAtomLearning;
 import tsetlin.MultivariateConvolutionalAutomatonMachine;
 
 public class TimeSeriesClassification {
@@ -130,7 +131,7 @@ public class TimeSeriesClassification {
 		int nClasses = 3;
 		
 		
-		MultivariateConvolutionalAutomatonMachine conv = new MultivariateConvolutionalAutomatonMachine(evolve.getConv_encoder(), threshold, nClasses, nClauses, max_specificity, true, 0f); 
+		AutomataAtomLearning conv = new AutomataAtomLearning(evolve.getConv_encoder(), threshold, nClasses, nClauses, max_specificity, true, 0f); 
 		
 		
 		int n_train = (int)(input_data.size()*.60);
